16db7ac294ac81367464c33c3245540ea074fb1e
Max <maksymilian.dabrowski93@gmail.com>
2019-03-31 23:27:24 +0200

    Updates

---
+++ precache-manifest.937bb0b1e866088cc255db2d15bfd16e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2f53484af876a080df3d",
+    "url": "/ToDo-App-with-React/static/css/main.e660b6d8.chunk.css"
+  },
+  {
+    "revision": "2f53484af876a080df3d",
+    "url": "/ToDo-App-with-React/static/js/main.2f53484a.chunk.js"
+  },
+  {
+    "revision": "f6b84cc0c44b8101ab3f",
+    "url": "/ToDo-App-with-React/static/js/1.f6b84cc0.chunk.js"
+  },
+  {
+    "revision": "82363a783bf257e39f7f",
+    "url": "/ToDo-App-with-React/static/js/runtime~main.82363a78.js"
+  },
+  {
+    "revision": "f026d23c1489c8d6cfb08432abdee0ac",
+    "url": "/ToDo-App-with-React/index.html"
+  }
+];
